b6cf132c1deb163137740b1938d3e20b72261ec9
MyUsername <figusgiacomo04@gmail.com>
2024-02-06 15:39:29 +0100

    to-fix: dont appear text

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
-<script setup>
+<script>
+
+    import AppTitle from  "@/components/AppTitle.vue";
+
+    export default{
+        components: {
+            AppTitle
+        }
+    };
 
 </script>
 
 <template>
-  
+    <div>
+        <AppTitle/>
+    </div>
 </template>
 
 <style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+    name: AppTitle;
+</script>
+
+<template>
+  <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style >
+    h1{
+        color: red;
+    }
+</style>
